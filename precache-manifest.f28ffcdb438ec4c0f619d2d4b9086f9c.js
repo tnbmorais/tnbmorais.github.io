ff189186216f44608e8ac16bee18829a91e2e9ac
Tiago Morais <tiago.morais@blip.pt>
2019-01-11 15:51:28 +0000

    Add TUST project

---
+++ precache-manifest.f28ffcdb438ec4c0f619d2d4b9086f9c.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = [
+  {
+    "url": "/static/js/vendor.b6f73dd30d2136a08a88.js"
+  },
+  {
+    "url": "/static/js/manifest.2ae2e69a05c33dfc65f8.js"
+  },
+  {
+    "url": "/static/js/app.92bb29c58a816b88391f.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/fonts/MaterialIcons-Regular.e79bfd8.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/fonts/MaterialIcons-Regular.a37b0c0.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/fonts/MaterialIcons-Regular.570eb83.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/fonts/MaterialIcons-Regular.012cf6a.woff"
+  },
+  {
+    "revision": "92bb29c58a816b88391f",
+    "url": "/static/css/app.4f01c8a6c2788a3e923c370139bbe90f.css"
+  }
+];
